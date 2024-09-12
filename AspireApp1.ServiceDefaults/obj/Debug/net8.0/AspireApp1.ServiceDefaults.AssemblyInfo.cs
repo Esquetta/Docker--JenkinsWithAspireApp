8c3e20d87d2a33db861c979ead11a896972b543c
Furkan <esquetta@gmail.com>
2024-09-12 21:15:50 +0300

    Docker Files updated.

--- AspireApp1.ServiceDefaults/obj/Debug/net8.0/AspireApp1.ServiceDefaults.AssemblyInfo.cs
+++ AspireApp1.ServiceDefaults/obj/Debug/net8.0/AspireApp1.ServiceDefaults.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AspireApp1.ServiceDefaults")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07810e5c1d4da9a477b5cebe1919758ada16f08b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89f70f820e72d2fc56fc16cbe82fe911227860ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("AspireApp1.ServiceDefaults")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AspireApp1.ServiceDefaults")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
